0a7ad9df6284488c119d410222fa752f78125ecc
Jose <al_jperaltaa@iessierradelasvillas.org>
2023-01-23 11:15:25 +0100

    Lista a medias

--- Seleccion_Equipos.py
+++ Seleccion_Equipos.py
@@ -1,11 +1,15 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title="SELECCION De Equipo"
+    page.title="Seleccion de equipos"
+
+    vEquipos= ["Aston Villa", "Man-United", "Juventus", "Real Madrid", "Al_Nassr"]
+    vEquiposSeleccionados=[]
 
     def cambiar_imagen(e):
         if (ddEquipo.value == "Aston Villa"):
             imagenEquipo.src = "Aston-Villa.png"
+
         elif (ddEquipo.value == "Man United"):
             imagenEquipo.src = "imagenes/Man-United.png"
             
@@ -34,25 +38,16 @@ def main(page: ft.Page):
 
     imagenEquipo = ft.Image(src="aa")
 
-
     page.add (ddEquipo,imagenEquipo)
 
-
-    #vEquipos= ["Aston Villa", "Man-United", "Juventus", "Real Madrid", "Al_Nassr"]
-    #menu=ft.Dropdown(hint_text="Selecciona un equipo",width=250,on_change=cambiar_imagen)
-    '''
     for equipo in vEquipos:
-        menu.options.append(ft.dropdown.Option(equipo))
+        ddEquipo.options.append(ft.dropdown.Option(equipo))
 
-    def GUARDAR():
-        cambiar_imagen = ft.dropdown.Option.get()
-        vEquipos.append(cambiar_imagen)
-    '''
+    #def GUARDAR():
+        #cambiar_imagen = ft.dropdown.Option.get()
+        #vEquipos.append(cambiar_imagen)
 
     #seleccionar_equipo=ft.FloatingActionButton(icon=ft.icons.ADD, on_click=GUARDAR)
     #page.add(seleccionar_equipo)
-    
-    
-
 
 ft.app(target=main,assets_dir="imagenes")
