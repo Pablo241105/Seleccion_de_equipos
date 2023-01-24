3f189e0218f108a2bb6c673e8fea1f7cb5f59766
Jose <al_jperaltaa@iessierradelasvillas.org>
2023-01-24 14:37:37 +0100

    PROYECTO TERMINADO

--- Seleccion_Equipos.py
+++ Seleccion_Equipos.py
@@ -3,10 +3,14 @@ import flet as ft
 def main(page: ft.Page):
     page.title="SELECCION De Equipo"
 
+    vEquipos= ["Aston Villa", "Man-United", "Juventus", "Real Madrid", "Al_Nassr"]
+    vEquiposSeleccionados=[]
+
     def cambiar_imagen(e):
         if (ddEquipo.value == "Aston Villa"):
             imagenEquipo.src = "Aston-Villa.png"
-        elif (ddEquipo.value == "Man United"):
+
+        elif (ddEquipo.value == "Man-United"):
             imagenEquipo.src = "imagenes/Man-United.png"
             
         elif (ddEquipo.value == "Juventus"):
@@ -21,38 +25,29 @@ def main(page: ft.Page):
         page.update()
     
     
-    ddEquipo = ft.Dropdown(label="Equipos",width=500,
-        options=[
-            ft.dropdown.Option("Aston Villa"),
-            ft.dropdown.Option("Man United"),
-            ft.dropdown.Option("Real Madrid"),
-            ft.dropdown.Option("Juventus"),
-            ft.dropdown.Option("Al-Nassr"),
-        ],
-        on_change=cambiar_imagen
-    )
+    ddEquipo = ft.Dropdown(label="Equipos",width=500,on_change=cambiar_imagen)
 
     imagenEquipo = ft.Image(src="aa")
 
 
     page.add (ddEquipo,imagenEquipo)
 
-
-    #vEquipos= ["Aston Villa", "Man-United", "Juventus", "Real Madrid", "Al_Nassr"]
-    #menu=ft.Dropdown(hint_text="Selecciona un equipo",width=250,on_change=cambiar_imagen)
-    '''
+    
     for equipo in vEquipos:
-        menu.options.append(ft.dropdown.Option(equipo))
+        ddEquipo.options.append(ft.dropdown.Option(equipo))
 
-    def GUARDAR():
-        cambiar_imagen = ft.dropdown.Option.get()
-        vEquipos.append(cambiar_imagen)
-    '''
+    def GUARDAR(e):
+        if vEquiposSeleccionados.count(ddEquipo.value)== 0:
+            vEquiposSeleccionados.append(ddEquipo.value)
+            print(vEquiposSeleccionados)
+        else:
+            dlg = ft.AlertDialog(title=ft.Text("EQUIPO REPETIDO!!!"))
+            page.dialog = dlg
+            dlg.open = True
+            page.update()
 
-    btn_seleccionar_equipo=ft.FloatingActionButton(icon=ft.icons.ADD)
+    btn_seleccionar_equipo=ft.FloatingActionButton(icon=ft.icons.ADD,on_click=GUARDAR)
     page.add(btn_seleccionar_equipo)
     
     
-
-
 ft.app(target=main,assets_dir="imagenes")
