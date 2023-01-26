235ae96e7d01aacb5aee15125d63bd2e53a1dcb2
Pablo241105 <al_prodriguezf@iessierradelasvillas.org>
2023-01-26 10:11:00 +0100

    List view hecho

--- Seleccion_Equipos.py
+++ Seleccion_Equipos.py
@@ -2,10 +2,10 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title="Seleccion de equipos"
-
     vEquipos= ["Aston Villa", "Man-United", "Juventus", "Real Madrid", "Al_Nassr"]
     vEquiposSeleccionados=[]
-
+    lv = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+    
     def cambiar_imagen(e):
         if (ddEquipo.value == "Aston Villa"):
             imagenEquipo.src = "Aston-Villa.png"
@@ -29,23 +29,27 @@ def main(page: ft.Page):
 
     for equipo in vEquipos:
         ddEquipo.options.append(ft.dropdown.Option(equipo))
-
+        
     imagenEquipo = ft.Image(src="aa")
 
     page.add (ddEquipo,imagenEquipo)
 
     def GUARDAR(e):
-        if vEquiposSeleccionados.count(ddEquipo.value)== 0:
+        sel=ddEquipo.value
+        if vEquiposSeleccionados.count(sel)== 0:
             vEquiposSeleccionados.append(ddEquipo.value)
             print(vEquiposSeleccionados)
+            lv.controls.append(ft.Text(sel))      
+            page.update()       
         else:
             dlg = ft.AlertDialog(title=ft.Text("EQUIPO REPETIDO!!!"))
             page.dialog = dlg
             dlg.open = True
             page.update()
 
-    btn_seleccionar_equipo=ft.FloatingActionButton(icon=ft.icons.ADD,on_click=GUARDAR)
-    page.add(btn_seleccionar_equipo)
     
+    btn_seleccionar_equipo=ft.FloatingActionButton(icon=ft.icons.ADD,on_click=GUARDAR)
+    page.add(btn_seleccionar_equipo,lv)
+ 
     
 ft.app(target=main,assets_dir="imagenes")
