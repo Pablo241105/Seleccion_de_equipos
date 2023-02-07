5d7ef60ccb9b8a0290519cb2bfaf554dd6900ae3
Jose <al_jperaltaa@iessierradelasvillas.org>
2023-02-07 14:40:44 +0100

    ARREGLAR PROGRAMA

--- Seleccion_Equipos.py
+++ Seleccion_Equipos.py
@@ -2,20 +2,22 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title="Seleccion de equipos"
-    vEquipos=[]
+    #vEquipos=["Aston Villa","Man-United","Juventus","Real Madrid","Al_Nassr"]
     vEquiposSeleccionados=[]
-    lv = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
 
-    def cargarEquipos():
-        vEquipos = []
-        f = open("Cargar_Equipos.txt","r")
+    def GUARDAR(e):
+        sel=ddEquipo.value
+        if vEquiposSeleccionados.count(sel)== 0:
+            vEquiposSeleccionados.append(sel)
+            print(vEquiposSeleccionados)
+            lv.controls.append(ft.Text(sel))      
+            page.update()       
+        else:
+            dlg = ft.AlertDialog(title=ft.Text("EQUIPO REPETIDO!!!"))
+            page.dialog = dlg
+            dlg.open = True
+            page.update()
 
-        for linea in f:
-            vEquipos.append(linea)
-
-        f.close()
-        return vEquipos
-  
     def cambiar_imagen(e):
         if (ddEquipo.value == "Aston Villa"):
             imagenEquipo.src = "Aston-Villa.png"
@@ -29,37 +31,37 @@ def main(page: ft.Page):
         elif (ddEquipo.value == "Real Madrid"):
             imagenEquipo.src = "imagenes/Real Madrid.png"
            
-        else:
-            imagenEquipo.src = "Al_Nassr.png"
-          
+        elif (ddEquipo.value == "Al_Nassar"):
+            imagenEquipo.src = "imagenes/Al_Nassr.png"
+
+        else: 
+            imagenEquipo.src = "imagenes/Sin_foto.jpg"
+        
         page.update()
     
-    
-    ddEquipo = ft.Dropdown(label="Equipos",width=500,on_change=cambiar_imagen)
+    def cargarEquipos():
+        vEquipos = []
+        f = open("Cargar_Equipos.txt","r")
 
-    for equipo in vEquipos:
-        ddEquipo.options.append(ft.dropdown.Option(equipo))
-        
+        linea = f.readline()
+        vEquipos = linea.split(sep=";")
+
+        f.close()
+        return vEquipos
+
+
+
     imagenEquipo = ft.Image(src="aa")
-
-    page.add (ddEquipo,imagenEquipo)
-
-    def GUARDAR(e):
-        sel=ddEquipo.value
-        if vEquiposSeleccionados.count(sel)== 0:
-            vEquiposSeleccionados.append(ddEquipo.value)
-            print(vEquiposSeleccionados)
-            lv.controls.append(ft.Text(sel))      
-            page.update()       
-        else:
-            dlg = ft.AlertDialog(title=ft.Text("EQUIPO REPETIDO!!!"))
-            page.dialog = dlg
-            dlg.open = True
-            page.update()
-
-    
     btn_seleccionar_equipo=ft.FloatingActionButton(icon=ft.icons.ADD,on_click=GUARDAR)
     page.add(btn_seleccionar_equipo,lv)
- 
-    
+    lv = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+    ddEquipo = ft.Dropdown(label="Equipos",width=500,on_change=cambiar_imagen)
+
+    vEquipos=cargarEquipos()
+    print(vEquipos)
+    for equipo in vEquipos:
+        ddEquipo.options.append(ft.dropdown.Option(equipo))
+
+
+    page.add (ddEquipo,imagenEquipo,btn_seleccionar_equipo,lv)
 ft.app(target=main,assets_dir="imagenes")
